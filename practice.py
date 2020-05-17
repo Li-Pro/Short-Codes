6ab016cf9fb14fa6e2c05708d6ec3b21d2b4fd53
Li-Pro <53344765+Li-Pro@users.noreply.github.com>
2020-05-17 16:11:17 +0800

    Updating practice.py

--- practice.py
+++ practice.py
@@ -1,29 +1,96 @@
 import tkinter as tk
+import math
+
+#------------------------ Game Objects ------------------------------
+
+class GameObject:
+	def __init__(self, game):
+		self.theGame = game
+	
+	def initDraw(self):
+		return
+	
+	def update(self):
+		return
+
+class Player(GameObject):
+	speed = 10
+	def __init__(self, game):
+		super().__init__(game)
+		
+		self.posX = game.WIDTH/2
+		self.posY = game.HEIGHT/2
+		
+		self.lastVX = self.speed
+		self.lastVY = 0
+	
+	def initDraw(self):
+		canvas = self.theGame.canvas
+		posX, posY = self.posX, self.posY
+		self.playerId = canvas.create_rectangle(posX-8, posY-8, posX+8, posY+8, fill='#ffd900')
+	
+	def move(self, vx=0, vy=0):
+		if (not vx) and (not vy):
+			return
+		
+		unitLen = math.sqrt(vx*vx + vy*vy)
+		modUnit = self.speed / unitLen
+		
+		vx *= modUnit
+		vy *= modUnit
+		
+		self.lastVX = vx
+		self.lastVY = vy
+		
+		self.posX += vx
+		self.posY += vy
+		
+		canvas = self.theGame.canvas
+		canvas.move(self.playerId, vx, vy)
+
+class Bullet(GameObject):
+	speedMod = 5
+	def __init__(self, game, player):
+		super().__init__(game)
+		
+		self.posX = player.posX
+		self.posY = player.posY
+		
+		self.vX = player.lastVX * self.speedMod
+		self.vY = player.lastVY * self.speedMod
+	
+	def initDraw(self):
+		canvas = self.theGame.canvas
+		posX, posY = self.posX, self.posY
+		vX, vY = self.vX, self.vY
+		
+		bound1 = [posX, posY]
+		bound2 = [posX + vX/5, posY + vY/5]
+		for i in range(2):
+			if bound1[i] < bound2[i]:
+				bound1[i] -= 1
+				bound2[i] += 1
+			else:
+				bound1[i] += 1
+				bound2[i] -= 1
+		
+		self.objId = canvas.create_oval(bound1[0], bound1[1], bound2[0], bound2[1], fill='#b7ff30')
+	
+	def update(self):
+		canvas = self.theGame.canvas
+		canvas.move(self.objId, self.vX, self.vY)
+
+#--------------------------------------------------------------------
+
 
 class Game:
-	class Player:
-		def __init__(self, game):
-			self.posX = game.WIDTH/2
-			self.posY = game.HEIGHT/2
-			
-			self.theGame = game
-		
-		def initDraw(self):
-			canvas = self.theGame.canvas
-			posX, posY = self.posX, self.posY
-			self.playerId = canvas.create_rectangle(posX-5, posY-5, posX+5, posY+5, fill='#ffd900')
-		
-		def move(self, vx=0, vy=0):
-			canvas = self.theGame.canvas
-			canvas.move(self.playerId, vx, vy)
-	
 	WIDTH, HEIGHT = 800, 600
 	def __init__(self, root):
 		self.root = root
 		
 		canvas = tk.Canvas(root, width=Game.WIDTH, height=Game.HEIGHT, background='#000000')
 		self.canvas = canvas
-		self.player = Game.Player(self)
+		self.player = Player(self)
 		self.objects = []
 		
 		self.initDraw()
@@ -40,6 +107,7 @@ class Game:
 		self.root.bind('<KeyRelease>', self.onKeyReleased)
 	
 	def onKeyPressed(self, event):
+		# print('#', dir(event), ord(event.char))
 		key = event.char.lower()
 		self.keyTyped.add(key)
 		
@@ -62,13 +130,21 @@ class Game:
 				vx -= 5
 			elif key == 'd':
 				vx += 5
+			
+			if key == ' ':
+				self.addObj(Bullet(self, player))
 		
 		self.player.move(vx, vy)
 		
+		# objects update
+		for obj in self.objects:
+			obj.update()
+		
 		self.root.after(20, self.onUpdate)
 	
-	def addObj(self):
-		return
+	def addObj(self, obj):
+		self.objects.append(obj)
+		obj.initDraw()
 	
 	def render():
 		return
