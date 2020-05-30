a8bdde2d43f1574de8ad598d423a11ceaadedb2c
Li-Pro <53344765+Li-Pro@users.noreply.github.com>
2020-05-30 22:43:31 +0800

    Updating topic2.py

--- topic2.py
+++ topic2.py
@@ -13,8 +13,7 @@ def countMax(seq):
 	
 	return mxn
 
-def test1(N, M):
-	""" Fixed N, with M growing. """
+def countCollid(N, M):
 	cnt = {}
 	for i in range(M):
 		x = random.randrange(0, N)
@@ -23,16 +22,37 @@ def test1(N, M):
 		
 		cnt[x] += 1
 	
-	return cnt
+	return countMax(cnt)
+
+def test1(bucket=1000):
+	""" Fixed N, with M growing. """
+	return [countCollid(bucket, i) for i in range(1, bucket)]
+
+def runSubTest(testfunc, bucket, estf=lambda x: x+3, estlabel='O(N)'):
+	T = testfunc(bucket)
+	sumt, avgt = 0, 0
+	for i in range(1, bucket):
+		x = T[i-1]
+		sumt += x
+		avgt += x / estf(i)
+	
+	print('Result: sum({tfunc}) = {sumt}, sum({tfunc}/{est}) = {avgt}'
+		.format(tfunc=testfunc.__name__, sumt=sumt, avgt=avgt, est=estlabel))
 
 def runTest():
-	st1, st1avg = 0, 0
-	for i in range(1, 1000):
-		x = countMax(test1(1000, i))
-		st1 += x
-		st1avg += x / (3 * i * math.log2(i+1))
+	F_NlogN = lambda i: (i+3) * math.log2(i+3)
 	
-	print('sum(test1) = {}, sum(test1/O(NlogN)) = {}'.format(st1, st1avg))
+	bucket_size = 1000
+	runSubTest(test1, bucket_size, F_NlogN, estlabel='O(NlogN)')
+	
+	# T1 = test1(bucket_size)
+	# st1, st1avg = 0, 0
+	# for i in range(1, bucket_size):
+		# x = T1[i-1]
+		# st1 += x
+		# st1avg += x / (2 * (i+3) * math.log2(i+3))
+	
+	# print('sum(test1) = {}, sum(test1/O(NlogN)) = {}'.format(st1, st1avg))
 
 
 #-------------------------- Testing Part ----------------------------
@@ -67,11 +87,10 @@ else:
 		currMod = sys.modules[__name__]
 		importlib.reload(currMod)
 		
-		# print('hah', {name: getattr(currMod, name) for name in __all__})
 		glob.update({name: getattr(currMod, name) for name in __all__})
 
-	# plotGraph plotting arguments
-	pltTest1 = dict(func=lambda x: countMax(test1(1000, x)), domain=(1, 1000), precision=1)
+	## plotGraph plotting arguments ##
+	pltTest1 = dict(func=lambda i: countCollid(bucket, i), domain=(1, 1000), precision=1)
 	
 	__all__ = ['reload', 'pltTest1']
 
