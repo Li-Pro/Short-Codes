d226d35be7c3fa474fee69e67d105d662981e349
Li-Pro <53344765+Li-Pro@users.noreply.github.com>
2020-05-31 16:53:50 +0800

    Updating utilPythonCode.py

--- utilPythonCode.py
+++ utilPythonCode.py
@@ -11,46 +11,73 @@ class _namedFunction:
 		self.__func = func
 		self.__label = label
 		
-		self.__docs = 'No help entry found for this command.'
+		self.docs = 'No help entry found for this command.'
 	
 	def __repr__(self):
 		return '{}\n'.format(self.__label)
 	
-	def __call__(self):
-		return self.__func()
+	def __call__(self, *args, **kwargs):
+		return self.__func(*args, **kwargs)
 	
 	def setHelpDoc(self, theDoc):
-		self.__docs = theDoc
+		self.docs = theDoc
 		return self
 
-_currMod = _modules[__name__]
-def _reloadModule(theMod, fromList=[], toNamespace=_currMod):
-	"""
-	Reload a module.
-	"""
+def _reloadModule(theMod, fromList=[], toNamespace=globals()):
 	if type(theMod) == str:
 		theMod = _modules[theMod]
 	
 	_reload(theMod)
+	
 	if type(toNamespace).__name__ == 'module':
 		toNamespace = vars(toNamespace)
 	
+	fromNamespace = vars(theMod)
+	for name in fromList:
+		toNamespace[name] = fromNamespace[name]
+	
+	return theMod
 
-def _loadHelp():
-	return
+_clear_doc = \
+"""
+clear() clears the screen (with 100 empty lines).
+""".strip()
+clear = _namedFunction(lambda: print('\n'*100), 'Type clear() to clean the screen.').setHelpDoc(_clear_doc)
 
-clear = _namedFunction(lambda: print('\n'*100), 'Type clear() to clean the screen.')
-reload = _namedFunction(_reloadModule, 'Type reload(...) to reload a module, help(reload) for help.').setHelpDoc(_reloadModule.__doc__.strip())
+_reload_doc = \
+"""
+Reload a module.
+Parameters:
+	theMod - the module (str / module).
+	
+	Optional Parameters:
+		fromList - the list to import (from <theMod> import <fromList>).
+		toNamespace - when <fromList> is specified, save the import names into <toNamespace> (usually globals() / locals()).
+	
+	Examples:
+		reload(math)
+		reload(math, fromList=['sin'], toNamespace=globals())
+""".strip()
+reload = _namedFunction(_reloadModule, 'Type reload(...) to reload a module, help(reload) for help.').setHelpDoc(_reload_doc)
+
 
 _help_msg = \
 """
 Type help_extra(<obj>) for help about command <obj>.
 List of extra commands:
+	help_extra - show help about console add-on commands.
 	clear - clean the screen.
 	reload - reload a module.
 """.strip()
 
-help_extra = _namedFunction(lambda: print(_help_msg +'\n'), _help_msg)
+def _loadHelp(obj=None):
+	if obj == None:
+		print(_help_msg +'\n')
+	else:
+		assert(type(obj) == _namedFunction)
+		print(obj.docs +'\n')
+
+help_extra = _namedFunction(_loadHelp, _help_msg).setHelpDoc(_help_msg)
 
 __all__ = ['clear', 'reload', 'help_extra']
 
